94203ca36f82015ae5d34410479e5eca84265ba7
koharu <82393007+koharun@users.noreply.github.com>
2024-05-26 22:11:24 +0900

    install expo router まで

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text} from 'react-native'
+
+const Index = (): JSX.Element => {
+    return(
+        <View>
+            <Text>
+                HelloWorld
+            </Text>
+        </View>
+    )
+} 
+
+export default Index
